186681a72502097dea641567a7b9d79d3a2c88d2
Amogh <amoghsdaddi@gmail.com>
2023-08-25 14:16:34 +0530

    initial commit

---
+++ src/main/java/com/agathsyatechnologies/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.agathsyatechnologies.repository;
+
+import com.agathsyatechnologies.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+}
